5c9f71b45f437dd881c4268e3cb3576d1a8fbca1
Parsa <mehdipourparsa@gmail.com>
2024-10-03 17:20:38 -0400

    feat : add base handlers

---
+++ DemoLibrary/BaseHandlers/SupportHandler.cs
@@ -0,0 +1,29 @@
+﻿using DemoLibrary.Interfaces;
+
+namespace DemoLibrary.BaseHandlers;
+
+/// <summary>
+/// The default chaining behavior can be implemented inside a base handler class.
+/// </summary>
+public abstract class SupportHandler : ISupportHandler
+{
+    protected ISupportHandler nextHandler;
+
+    public virtual void HandleRequest(SupportRequest request)
+    {
+        if (nextHandler != null)
+        {
+            nextHandler.HandleRequest(request);
+        }
+        else
+        {
+            Console.WriteLine("Request cannot be handled.");
+        }
+    }
+
+    public ISupportHandler SetNext(ISupportHandler handler)
+    {
+        nextHandler = handler;
+        return handler;
+    }
+}
