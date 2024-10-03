64b3513847bafec9a42826c67d771dac5062d3af
Parsa <mehdipourparsa@gmail.com>
2024-10-03 17:20:59 -0400

    feat : add support request

---
+++ DemoLibrary/SupportRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace DemoLibrary;
+
+public class SupportRequest
+{
+    public string Description { get; set; }
+    public RequestCategory Category { get; set; }
+
+    public SupportRequest(string description, RequestCategory category)
+    {
+        Description = description;
+        Category = category;
+    }
+}
+
+// Support request class
+public enum RequestCategory
+{
+    GeneralInquiry,
+    TechnicalIssue,
+    BillingProblem
+}
