fafc9b00a6eae83b1b226db43aeb86b3aa956b83
Parsa <mehdipourparsa@gmail.com>
2024-10-03 17:20:29 -0400

    feat : add interfaces

---
+++ DemoLibrary/Interfaces/ISupportHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoLibrary.Interfaces;
+
+/// <summary>
+/// The Handler interface declares a method for building the chain of
+/// handlers. It also declares a method for executing a request.
+/// </summary>
+public interface ISupportHandler
+{
+    void HandleRequest(SupportRequest request);
+    ISupportHandler SetNext(ISupportHandler handler);
+}
